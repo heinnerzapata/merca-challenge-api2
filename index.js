e9b1fca37183189a6586fba515baf17c7a3d3f14
Heinner <heinnerzapata1991@gmail.com>
2017-09-10 16:59:00 -0500

    v16

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express()
 
 var bodyParser = require('body-parser')
-var request = require('request')
+//var request = require('request')
 //var config = require('./config');
 
 var port = process.env.PORT || 3002
