ba6969d9a96d322f3612fb3750071d8730839faf
Heinner <heinnerzapata1991@gmail.com>
2017-09-10 16:54:41 -0500

    v13

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var express = require("express");
-var app = express();
+var express = require("express")
+var app = express()
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 //var request = require('request');
 //var config = require('./config');
 
-var port = process.env.PORT || 3002;
+var port = process.env.PORT || 3002
 
 //app.use(bodyParser.urlencoded({ extended : false }))
 //app.use(bodyParser.json())
 
 app.get("/" , function(req , res){
   res.send("Hello from nodejs and express");
-});
+})
 
-console.log("web application opened");
+console.log("web application opened")
 //app.listen(process.env.PORT || 3003);
 
 app.listen(port, () => {
