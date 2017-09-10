bfd10b638df689dffa870fe5f488f11493a4628b
Heinner <heinnerzapata1991@gmail.com>
2017-09-10 16:47:34 -0500

    t6

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
 
+const port = process.env.PORT || 3002;
+
 app.get("/" , function(req , res){
   res.send("Hello from nodejs and express");
 });
 
 console.log("web application opened");
-app.listen(process.env.PORT || 3003);
+//app.listen(process.env.PORT || 3003);
+
+app.listen(port, () => {
+  console.log(`API REST running in http://localhost:${port}`)
+})
